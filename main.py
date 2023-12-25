36c6a24a718d6ad481743e7cb242e448af38a6a1
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-12-25 19:42:42 -0300

    refactor: Organizing Configuration Files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from app.excel.SpreadsheetGenerator import SpreadsheetGenerator
+from app.email.EmailApp import send_email
 
 stores = [2, 6, 8, 10, 11, 12, 15, 16, 20, 21, 22,
           23, 24, 25, 29, 30, 35, 37, 47, 48, 49,
@@ -11,4 +12,4 @@ generator = SpreadsheetGenerator(stores)
 
 # generator.generate_spreadsheets('./data/24-12-23_Cancelados.xlsx', '24-12-23')
 
-# send_email()
+send_email()
