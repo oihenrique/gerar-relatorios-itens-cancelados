f636584c3e3ea8851f25ced04817a391196cb1d5
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-12-26 11:08:54 -0300

    feat: created a user interface

---
+++ gui/main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  win.loadFile('index.html')
+}
+// ...
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
+
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+  })
+
