7e4272cb9016e72f4b754b215294ec781d5bb8d1
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-12-28 12:26:40 -0300

    feat: add app icon

--- gui/main.js
+++ gui/main.js
@@ -5,6 +5,7 @@ const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: './assets/icon.png',
     resizable: false,
     autoHideMenuBar: true,
     webPreferences: {
